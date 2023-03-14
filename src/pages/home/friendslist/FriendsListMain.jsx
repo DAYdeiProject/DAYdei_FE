9115026b05ddd1372ed9792f5e2cdbc7b7b4ba77
juninkorea95 <juninkorea95@gmail.com>
2023-03-14 11:33:09 +0900

    Design | 친구목록 페이지 친구리스트/구독 리스트 영역 나누기

--- src/pages/home/friendslist/FriendsListMain.jsx
+++ src/pages/home/friendslist/FriendsListMain.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import styled from "styled-components";
+import { CalendarWrapper } from "../calendar/CalendarMain";
 
 function FriendsListMain() {
-  return <div>FriendsListMain</div>;
+  return (
+    <>
+      <CalendarWrapper>
+        <WholeAreaWrapper>
+          <ListsHeader>리스트 헤더입니다.</ListsHeader>
+          <ListsBody>
+            <ListFrame>친구리스트</ListFrame>
+            <ListFrame>구독리스트</ListFrame>
+          </ListsBody>
+        </WholeAreaWrapper>
+      </CalendarWrapper>
+      ;
+    </>
+  );
 }
+
+const WholeAreaWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ListsHeader = styled.div`
+  height: 43px;
+  margin-bottom: 30px !important;
+`;
+
+const ListsBody = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const ListFrame = styled.div`
+  height: 100%;
+  width: 49%;
+`;
 
 export default FriendsListMain;
