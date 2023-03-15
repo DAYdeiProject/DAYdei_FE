0070dff6ade59a5620b32d2555d4ae03ed23dc16
dajeong <daaa_j@naver.com>
2023-03-15 20:22:02 +0900

    rename : 파일 재추가

--- src/elements/Modalwrapper.jsx
+++ src/elements/Modalwrapper.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const ModalWrapper = ({ children }) => {
+  //
   return (
     <ModalOverlay>
       <ModalWrap>{children}</ModalWrap>
