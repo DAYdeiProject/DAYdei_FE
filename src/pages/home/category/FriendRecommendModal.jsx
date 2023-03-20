a297d17dad448049d907c52d6d86bac5e3f10707
juninkorea95 <juninkorea95@gmail.com>
2023-03-20 21:46:03 +0900

    Feat | famousList get요청 통신작업중

--- src/pages/home/category/FriendRecommendModal.jsx
+++ src/pages/home/category/FriendRecommendModal.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import ModalWrap from "../../../elements/ModalWrap";
 import Modal from "../../../elements/Modal";
 import { ModalContent } from "./CategoryModal";
+import { useDispatch, useSelector } from "react-redux";
+import { __getFamousList } from "../../../redux/modules/friendsSlice";
 
 function FriendRecommendModal({ setShowFriendRecommendModal, setIsModalVisible }) {
+  const FamousList = useSelector((state) => state.friends.FamousList);
+  console.log(FamousList);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(__getFamousList());
+  });
+
   return (
     <>
       <ModalWrap>
