f4644413d5be2ae5e5f45540def1489216eb818d
dajeong <daaa_j@naver.com>
2023-04-08 01:01:09 +0900

    fix : localstorage user정보 암호화

--- src/utils/cookie/userInfo.js
+++ src/utils/cookie/userInfo.js
@@ -1,18 +1,23 @@
 import Cookies from "js-cookie";
 import { useDispatch } from "react-redux";
 import { __getMyProfile } from "../../redux/modules/usersSlice";
+import { decrypt, encrypt } from "./encryption";
 
 // 로컬스토리지 userId 설정
 function SetUserInfo(token, id) {
+  const secretKey = `${process.env.REACT_APP_LOCAL_SECRETKEY}`;
   // 토큰 설정
   const expiryDate = new Date(Date.now() + 6 * 60 * 60 * 1000);
   Cookies.set("accessJWTToken", token, { expires: expiryDate });
 
   // userId 설정
   const userInfo = {
-    userId: id,
+    userId: String(id),
   };
-  localStorage.setItem("userInfo", JSON.stringify(userInfo));
+
+  // userId 암호화
+  const encryptUser = encrypt(userInfo);
+  localStorage.setItem(secretKey, encryptUser);
 
   return null;
 }
@@ -20,13 +25,16 @@ export default SetUserInfo;
 
 // 로컬스토리지 userId 가져오기
 export function GetUserInfo() {
-  const localUserInfo = localStorage.getItem("userInfo");
-  let userInfo = "";
-  if (localUserInfo) {
-    userInfo = JSON.parse(localUserInfo);
-  }
+  const secretKey = `${process.env.REACT_APP_LOCAL_SECRETKEY}`;
+  const localUserInfo = localStorage.getItem(secretKey);
 
-  return userInfo;
+  try {
+    const result = decrypt(localUserInfo);
+    return result;
+  } catch (error) {
+    console.error("로컬 저장소 데이터 복호화 중 오류가 발생했습니다. : ", error);
+    return null;
+  }
 }
 
 // parma id의 유저 있는지 확인
@@ -35,6 +43,6 @@ export function UserCheck(id) {
   let result = "";
 
   dispatch(__getMyProfile(id)).then((data) => {
-    console.log("함수에서~~~~", data);
+    //console.log("함수에서~~~~", data);
   });
 }
